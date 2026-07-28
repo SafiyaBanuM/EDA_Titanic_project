3e1891c46bbf2b5b9149d3fce29192215208f870
SafiyaBanuM <msafiyabanu@gmail.com>
2026-07-28 09:59:46 +0530

    Refactor EDA.py for improved readability

--- EDA.py
+++ EDA.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load Dataset
-df = pd.read_csv(r"C:/Users/SAFIYA BANU/Downloads/titanic.csv")
+df = pd.read_csv(r"titanic.csv")
 
 # Display first 5 rows
 print(df.head())
